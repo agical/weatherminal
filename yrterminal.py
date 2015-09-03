790feee29248f2ffb8c30c35308f741685c41729
Stefan Östergaard <stefan.ostergaard@gmail.com>
2015-09-03 10:59:32 +0200

    Can pick temperature from data

--- yrterminal.py
+++ yrterminal.py
@@ -1,7 +1,19 @@
 
 import pickle
 
-with open('example-forecast.dat', 'rb') as f:
-    forecast = pickle.loads(f.read())
 
-print(forecast)
+def pick_hour_data(forecast):
+    innerforecast = forecast['weatherdata']['forecast']
+    hour_by_hour_data = innerforecast['tabular']['time']
+    return [(hour_data['@from'], hour_data['temperature']['@value'])
+            for hour_data in hour_by_hour_data]
+
+
+def read_example():
+    with open('example-forecast.dat', 'rb') as f:
+        return  pickle.loads(f.read())
+
+hour_by_hour_forecast = read_example()
+hour_data = pick_hour_data(hour_by_hour_forecast)
+#print(hour_by_hour_forecast)
+
