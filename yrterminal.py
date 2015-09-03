16f3d62d296b8fb889d88660d3941546bbc44b13
Stefan Östergaard <stefan.ostergaard@gmail.com>
2015-09-03 11:22:25 +0200

    Can format forecast with hours only

--- yrterminal.py
+++ yrterminal.py
@@ -1,5 +1,8 @@
 
 import pickle
+import shutil
+
+
 
 
 def pick_hour_data(forecast):
@@ -13,7 +16,23 @@ def read_example():
     with open('example-forecast.dat', 'rb') as f:
         return  pickle.loads(f.read())
 
+    
+def pick_hour(instant):
+    return instant[11:13]
+
+def format_cell(data):
+    return "{:>4}".format(data)
+
+def format_forecast(hour_data):
+    columns = shutil.get_terminal_size().columns
+    
+    return "".join([format_cell(pick_hour(instant)) for instant, temp in hour_data])[0:columns]
+
+
+
+
 hour_by_hour_forecast = read_example()
 hour_data = pick_hour_data(hour_by_hour_forecast)
+
 #print(hour_by_hour_forecast)
 
